b2f598b13292cc9b5b0fd7468724b8b5944bcc40
3ricz7 <emdias@id.uff.br>
2026-05-01 12:40:19 -0300

    Codigo função encadeamento

---
+++ TABB.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct arvbinbusca{
+  int info;
+  struct arvbinbusca *esq, *dir, *prox;
+}TABB;
+
+TABB *TABB_inicializa(void);
+TABB *TABB_cria(int raiz, TABB *esq, TABB *dir);
+
+void TABB_imp_pre(TABB *a);
+void TABB_imp_pos(TABB *a);
+void TABB_imp_sim(TABB *a);
+void TABB_imp_ident(TABB *a);
+
+void TABB_libera(TABB *a);
+
+TABB *TABB_busca(TABB *a, int elem);
+
+TABB *TABB_insere(TABB *a, int elem);
+TABB *TABB_retira(TABB *a, int info);
+
+int *maioresN(TABB *a, int N, int *tam_vet);
+void printv(int* vet, int tam);
+TABB* encadeador(TABB* a);
+void encadeador_aux(TABB* a, TABB* viz);
+void imprime_lista(TABB* a);
